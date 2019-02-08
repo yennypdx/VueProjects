d79a89b7f5c880fd4369d8ad4e66b6039dcb4259
ypurwantopdx <yenny.purwanto@oit.edu>
2019-02-08 08:28:49 -0800

    added script
    added: removeif and endremoveif, inject and endinjectcss, copy html file to app folder in appTwo folder.

--- Week3Lab/appTwo/gulpfile.js
+++ Week3Lab/appTwo/gulpfile.js
@@ -10,50 +10,49 @@ var sourcemaps = require('gulp-sourcemaps');
 var watch = require('gulp-watch');
 
 let vendorPackages = [
-  'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/jquery/dist/jquery.min.js',
 ];
 
 function html() {
-  return src(['app/*.html'])
-      .pipe(inject(src(['./build/js/*.js', './build/**/*.css'], { read: false }),
-        {ignorePath: 'build', addRootSlash: false }))
-      .pipe(removeCode({ production: true }))
-      .pipe(dest('build'));
+    return src(['app/*.html'])
+        .pipe(inject(src(['./build/js/*.js', './build/**/*.css'], { read: false }), { ignorePath: 'build', addRootSlash: false }))
+        .pipe(removeCode({ production: true }))
+        .pipe(dest('build'));
 }
 
 function js() {
-  return src('app/js/*.js', { sourcemaps: true })
-    .pipe(sourcemaps.init())
-    .pipe(concat('app.min.js'))
-    .pipe(uglify())
-    .pipe(sourcemaps.write('.'))
-    .pipe(dest('build/js'));
+    return src('app/js/*.js', { sourcemaps: true })
+        .pipe(sourcemaps.init())
+        .pipe(concat('app.min.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/js'));
 }
 
 function vendor_js() {
-  return src(vendorPackages)
-    .pipe(concat('vendor.min.js'))
-    .pipe(dest('build/js'))
+    return src(vendorPackages)
+        .pipe(concat('vendor.min.js'))
+        .pipe(dest('build/js'))
 }
 
 function css() {
-  return src('app/css/*.css')
-    .pipe(sourcemaps.init())
-    .pipe(concat('app.min.css'))
-    .pipe(minifyCSS())
-    .pipe(sourcemaps.write('.'))
-    .pipe(dest('build/css'))
+    return src('app/style/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(concat('app.min.css'))
+        .pipe(minifyCSS())
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/css'))
 }
 
 function images() {
-  return src('app/images/*.jpg')
-    .pipe(dest('build/images'))
+    return src('app/image/*.jpg')
+        .pipe(dest('build/images'))
 }
 
 function watch_files() {
-  watch('app/*.html', html);
-  watch('app/js/*.js', js);
-  watch('app/css/*.css', css);
+    watch('app/*.html', html);
+    watch('app/js/*.js', js);
+    watch('app/css/*.css', css);
 }
 
 exports.js = js;
