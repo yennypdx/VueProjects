bdd5ce8506994033604326cb7c5bac8c0c9d90b0
ypurwantopdx <yenny.purwanto@oit.edu>
2019-02-16 07:43:33 -0800

    Week4Lab Commit
    Vue

---
+++ Week4Lab/webapp/src/components/Person.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <h1 class="section-heading" style="text-align:center">Person Information</h1>
+        <h2 class="section">Person List</h2>
+
+        <table align="center">
+            <thead>
+                <tr>
+                    <th style="text-align:center">First Name</th>
+                    <th style="text-align:center">Mid. Initial</th>
+                    <th style="text-align:center">Last Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="person in persons">
+                    <td>{{ person.fname }}</td>
+                    <td>{{ person.midInt }}</td>
+                    <td>{{ person.lname }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>    
+</template>
+
+<script>
+    export default {
+        name: 'Person',
+        mounted(){
+            this.persons = getPersons();
+        },
+        data(){
+            return{
+                persons: []
+            }
+        }
+    }
+
+    function getPersons(){
+        return JSON.parse(testPersons).persons;
+    }
+
+    var testPersons = '{"persons": [{"fname": "Roger","midInt": "A","lname": "Smith"}, {"fname": "Mark","midInt": "J","lname": "Hamilton"}, {"fname": "Brandon","midInt": "H","lname": "Cartwright"}, {"fname": "Glen","midInt": "M","lname": "Sullivan"}, {"fname": "Catherine","midInt": "S","lname": "Peterson"}]}'
+</script>
+
+<style scoped>
+.section-heading {
+    background-color: lightskyblue;
+}
+
+.section {
+    background-color: wheat;
+}
+</style>
---
+++ Week4Lab/webapp/src/components/Student.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <h1 class="section-heading" style="text-align:center">Student Information</h1>
+        <h2 class="section">Student List</h2>
+
+        <table align="center">
+            <thead>
+                <tr>
+                    <th style="text-align:center">Student ID</th>
+                    <th style="text-align:center">Email Address</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="student in students">
+                    <td>{{ student.id }}</td>
+                    <td>{{ student.email }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'Student',
+        mounted(){
+            this.students = getStudents();
+        },
+        data(){
+            return{
+                students: []
+            }
+        }
+    }
+
+    function getStudents(){
+        return JSON.parse(testStudents).students;
+    }
+
+    var testStudents = '{"students": [{"id": 95123654,"email": "roger.smith@oit.edu"}, {"id": 95131658,"email": "mark.hamilton@oit.edu"}, {"id": 95178549,"email": "brandon.cartwright@oit.edu"}, {"id": 95145625,"email": "glen.sullivan@oit.edu"}, {"id": 95133652,"email": "cat.peterson@oit.edu"}]}'
+</script>
+
+<style scoped>
+.section-heading {
+    background-color: lightskyblue;
+}
+
+.section {
+    background-color: wheat;
+}
+</style>
