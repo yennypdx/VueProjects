591c3e924f4bf988462ce3400466d6f22291a15d
ypurwantopdx <yenny.purwanto@oit.edu>
2019-02-25 00:30:11 -0800

    Modified Config file.
    Moving the end points from the dev.environment to prod.environment.

--- Week5Lab/webapp/config/dev.env.js
+++ Week5Lab/webapp/config/dev.env.js
@@ -3,7 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-    NODE_ENV: '"development"',
-    STUDENT_API: '"http://localhost:8081/api/students"',
-    PERSON_API: '"http://localhost:8081/api/persons"'
+    NODE_ENV: '"development"'
 })
--- Week5Lab/webapp/config/prod.env.js
+++ Week5Lab/webapp/config/prod.env.js
@@ -1,4 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+    NODE_ENV: '"production"',
+    STUDENT_API: '"http://localhost:8081/api/students"',
+    PERSON_API: '"http://localhost:8081/api/persons"'
 }
